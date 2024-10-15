861645f57575188846067f0b6fc99a353de7b28b
Redlyst <id.redlyst@gmail.com>
2024-10-15 18:58:06 +0900

    adding new handler create campaign

--- campaign/service.go
+++ campaign/service.go
@@ -53,7 +53,7 @@ func (s *service) CreateCampaign(input CreateCampaignInput) (Campaign, error) {
 	campaign.GoalAmount = input.GoalAmount
 	campaign.UserID = input.User.ID
 
-	slugCandidate := fmt.Sprintf("%s %s", input.Name, input.User.ID)
+	slugCandidate := fmt.Sprintf("%s %d", input.Name, input.User.ID)
 	campaign.Slug = slug.Make(slugCandidate)
 
 	newCampaign, err := s.repository.Save(campaign)
