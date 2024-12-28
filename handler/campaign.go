b5b8412b08a68416fe7c0566d6e7275ebae2b6f3
Redlyst <id.redlyst@gmail.com>
2024-12-28 21:34:52 +0900

    adding 'SaveCampaignImage' authorization to prevent as another user

--- handler/campaign.go
+++ handler/campaign.go
@@ -122,6 +122,10 @@ func (h *campaignHandler) UploadImage(c *gin.Context) {
 		return
 	}
 
+	currentUser := c.MustGet("currentUser").(user.User)
+	input.User = currentUser
+	userID := currentUser.ID
+
 	file, err := c.FormFile("file")
 	if err != nil {
 		data := gin.H{"is_uploaded": false}
@@ -130,9 +134,6 @@ func (h *campaignHandler) UploadImage(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, response)
 		return
 	}
-
-	currentUser := c.MustGet("currentUser").(user.User)
-	userID := currentUser.ID
 
 	path := fmt.Sprintf("images/%d-%s", userID, file.Filename)
 
